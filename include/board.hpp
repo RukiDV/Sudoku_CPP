c14448b4f49433a45b3bd4c5f5f97672a953031f
Rukiye Devran <ruki.dev@gmail.com>
2024-04-01 20:16:14 +0200

    add const getter to board

--- include/board.hpp
+++ include/board.hpp
@@ -13,11 +13,13 @@ class Board
 public:
     Board(uint32_t size_x, uint32_t size_y);
     void set_field(uint32_t x, uint32_t y, uint8_t value, ContentFlags flags);
-    std::pair<uint8_t, uint32_t> get_field(uint32_t x, uint32_t y);
+    std::pair<uint8_t, uint32_t> get_field(uint32_t x, uint32_t y) const;
     void set_value(uint32_t x, uint32_t y, uint8_t value);
-    uint8_t get_value(uint32_t x, uint32_t y);
+    uint8_t get_value(uint32_t x, uint32_t y) const;
     void set_flags(uint32_t x, uint32_t y, ContentFlags flags);
-    uint32_t get_flags(uint32_t x, uint32_t y);
+    uint32_t get_flags(uint32_t x, uint32_t y) const;
+    uint32_t get_size_x() const;
+    uint32_t get_size_y() const;
 
 private:
     uint32_t size_x = 0;
@@ -27,5 +29,6 @@ private:
     static const uint32_t value_mask = 0x000000ff;
 
     uint32_t& get_field_ref(uint32_t x, uint32_t y);
+    uint32_t get_field_content(uint32_t x, uint32_t y) const;
 };
 
--- src/board.cpp
+++ src/board.cpp
@@ -4,6 +4,12 @@
 Board::Board(uint32_t size_x, uint32_t size_y) : size_x(size_x), size_y(size_y), content(size_x * size_y, 0)
 {}
 
+uint32_t Board::get_field_content(uint32_t x, uint32_t y) const
+{
+    assert(x < size_x && y < size_y);
+    return content[size_x * y + x];
+}
+
 uint32_t& Board::get_field_ref(uint32_t x, uint32_t y)
 {
     assert(x < size_x && y < size_y);
@@ -16,7 +22,7 @@ void Board::set_field(uint32_t x, uint32_t y, uint8_t value, ContentFlags flags)
     get_field_ref(x, y) = uint32_t(value) | flags;
 }
 
-std::pair<uint8_t, uint32_t> Board::get_field(uint32_t x, uint32_t y)
+std::pair<uint8_t, uint32_t> Board::get_field(uint32_t x, uint32_t y) const
 {
     return std::make_pair<uint8_t, uint32_t>(get_value(x, y), get_flags(x, y));
 }
@@ -28,9 +34,9 @@ void Board::set_value(uint32_t x, uint32_t y, uint8_t value)
     get_field_ref(x, y) |= value;
 }
 
-uint8_t Board::get_value(uint32_t x, uint32_t y)
+uint8_t Board::get_value(uint32_t x, uint32_t y) const
 {
-    return get_field_ref(x, y) & value_mask;
+    return get_field_content(x, y) & value_mask;
 }
 
 void Board::set_flags(uint32_t x, uint32_t y, ContentFlags flags)
@@ -40,8 +46,18 @@ void Board::set_flags(uint32_t x, uint32_t y, ContentFlags flags)
     get_field_ref(x, y) |= flags;
 }
 
-uint32_t Board::get_flags(uint32_t x, uint32_t y)
+uint32_t Board::get_flags(uint32_t x, uint32_t y) const
 {
-    return get_field_ref(x, y) & flag_mask;
+    return get_field_content(x, y) & flag_mask;
+}
+
+uint32_t Board::get_size_x() const 
+{
+    return size_x;
+}
+
+uint32_t Board::get_size_y() const
+{
+    return size_y;
 }
 
